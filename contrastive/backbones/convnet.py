f6ed77275bfd2d5c5dd29edb27d0a3a4c49a5195
Joel Chavas <joel.chavas@gmail.com>
2022-10-21 12:39:49 +0200

    Gets normal dropout, as for aymeric branch

--- contrastive/backbones/convnet.py
+++ contrastive/backbones/convnet.py
@@ -4,68 +4,6 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils.checkpoint as cp
-
-from torch import Tensor
-
-class _DropoutNd(nn.Module):
-    __constants__ = ['p', 'inplace']
-    p: float
-    inplace: bool
-
-    def __init__(self, p: float = 0.5, inplace: bool = False) -> None:
-        super(_DropoutNd, self).__init__()
-        if p < 0 or p > 1:
-            raise ValueError("dropout probability has to be between 0 and 1, "
-                             "but got {}".format(p))
-        self.p = p
-        self.inplace = inplace
-
-    def extra_repr(self) -> str:
-        return 'p={}, inplace={}'.format(self.p, self.inplace)
-
-
-class Dropout3d_always(_DropoutNd):
-    r"""Randomly zero out entire channels (a channel is a 3D feature map,
-    e.g., the :math:`j`-th channel of the :math:`i`-th sample in the
-    batched input is a 3D tensor :math:`\text{input}[i, j]`).
-    Each channel will be zeroed out independently on every forward call with
-    probability :attr:`p` using samples from a Bernoulli distribution.
-
-    Alwyas applies dropout also during evaluation
-
-    As described in the paper
-    `Efficient Object Localization Using Convolutional Networks`_ ,
-    if adjacent pixels within feature maps are strongly correlated
-    (as is normally the case in early convolution layers) then i.i.d. dropout
-    will not regularize the activations and will otherwise just result
-    in an effective learning rate decrease.
-
-    In this case, :func:`nn.Dropout3d` will help promote independence between
-    feature maps and should be used instead.
-
-    Args:
-        p (float, optional): probability of an element to be zeroed.
-        inplace (bool, optional): If set to ``True``, will do this operation
-            in-place
-
-    Shape:
-        - Input: :math:`(N, C, D, H, W)` or :math:`(C, D, H, W)`.
-        - Output: :math:`(N, C, D, H, W)` or :math:`(C, D, H, W)` (same shape as input).
-
-    Examples::
-
-        >>> m = nn.Dropout3d(p=0.2)
-        >>> input = torch.randn(20, 16, 4, 32, 32)
-        >>> output = m(input)
-
-    .. _Efficient Object Localization Using Convolutional Networks:
-       https://arxiv.org/abs/1411.4280
-    """
-
-    def forward(self, input: Tensor) -> Tensor:
-        return F.dropout3d(input, self.p, True, self.inplace)
-
 
 class ConvNet(pl.LightningModule):
     r"""3D-ConvNet model class, based on
@@ -130,12 +68,11 @@ class ConvNet(pl.LightningModule):
                     kernel_size=3, stride=1, padding=1)))
             modules_encoder.append(('norm%s' %step, nn.BatchNorm3d(out_channels)))
             modules_encoder.append(('LeakyReLU%s' %step, nn.LeakyReLU()))
-            modules_encoder.append(('DropOut%s' %step, Dropout3d_always(p=drop_rate)))
             modules_encoder.append(('conv%sa' %step, nn.Conv3d(out_channels, out_channels,
                     kernel_size=4, stride=2, padding=1)))
             modules_encoder.append(('norm%sa' %step, nn.BatchNorm3d(out_channels)))
             modules_encoder.append(('LeakyReLU%sa' %step, nn.LeakyReLU()))
-            modules_encoder.append(('DropOut%sa' %step, Dropout3d_always(p=drop_rate)))
+            modules_encoder.append(('DropOut%sa' %step, nn.Dropout3d(p=drop_rate)))
             self.num_features = out_channels
         # flatten and reduce to the desired dimension
 
